109abe14072bc275f31709401913b7888ac1ead8
Manuel Kazaka <m.kazaka@gmail.com>
2024-05-08 17:55:21 -0700

    Repo

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,7 +1,10 @@
+// This makes VSCode check types as if you are using TypeScript
+//@ts-check
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react({include: '**/*.jsx'})],
+    server: {
+        hmr: true,
+        host: true,
+        strictPort: true,
+        watch: {
+            usePolling: true,
+        }
+    },
 })
